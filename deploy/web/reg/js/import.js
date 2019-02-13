e784178f89ed9b1a46528008e6108f952b741117
988game <1611679179@qq.com>
2019-02-13 16:08:47 +0800

    fix

--- deploy/web/reg/js/import.js
+++ deploy/web/reg/js/import.js
@@ -1,6 +1,6 @@
-var gameWebUrl = 'http://intest1.dstars.cc/minigame/guess/index.html'                                    //游戏web地址
-var funcReqUrl = 'http://intest.dstars.cc:8002/jccommon'               //接口地址
-var downloadUrl = 'http://intest.dstars.cc/club/ddtest/'
+var gameWebUrl = 'https://www.***.com/'                                    //游戏web地址
+var funcReqUrl = 'https://www.***.com/jccommon'               //接口地址
+var downloadUrl = 'https://d.***.com/'
 var defaultAppStr = 'jcapp'
 
 function getGameUrl() {
